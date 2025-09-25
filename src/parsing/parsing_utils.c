76f51c7b7f8cb7188a907b44c8d462ff58a6f8a9
O-A5M <akhmouchothman@gmail.com>
2025-09-25 16:56:10 +0100

    Working on the file content parsing

---
+++ src/parsing/parsing_utils.c
@@ -0,0 +1,32 @@
+#include "include.h"
+#include "libft.h"
+
+void	free_textures(t_textures **tex)
+{
+	free((*tex)->so);
+	free((*tex)->no);
+	free((*tex)->ea);
+	free((*tex)->we);
+	free((*tex));
+}
+
+void	free_map(t_map **map)
+{
+	if ((*map)->map)
+		free_array((*map)->map);
+	if ((*map)->textures)
+		free_textures(&(*map)->textures);
+	free((*map));
+}
+
+void	init_map(t_map **map)
+{
+	(*map)->map = NULL;
+	ft_memset((*map)->floore, -1, sizeof((*map)->floore));
+	ft_memset((*map)->ceilling, -1, sizeof((*map)->ceilling));
+	(*map)->textures = malloc(sizeof(t_textures));
+	(*map)->textures->ea = NULL;
+	(*map)->textures->no = NULL;
+	(*map)->textures->so = NULL;
+	(*map)->textures->we = NULL;
+}
