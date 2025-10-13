d1ca05a3134ce900fbce94d841c9c0088a21b4b9
O-A5M <akhmouchothman@gmail.com>
2025-10-13 16:12:16 +0100

    handling the no file given

--- main/main.c
+++ main/main.c
@@ -3,9 +3,17 @@
 int	main(int ac, char **av)
 {
 	(void)ac;
-	if (parse_file(av[1]))
-		printf("wrong\n");
+	if (ac == 2)
+	{
+		if (parse_file(av[1]))
+			printf("wrong\n");
+		else
+			printf("succes\n");
+	}
 	else
-		printf("succes\n");
+	{
+		printf("Please enter a valide file\n");
+		return (1);
+	}
 	return (0);
 }
