a86e780fd6076f05df5d5c6fe5f658284663f5b4
O-A5M <akhmouchothman@gmail.com>
2025-10-14 11:28:23 +0100

    fixing some errors

--- src/parsing/dynamic_array_utils.c
+++ src/parsing/dynamic_array_utils.c
@@ -1,52 +1,63 @@
-#include "include.h"
-#include "libft.h"
-#include <stdio.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   dynamic_array_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oakhmouc <oakhmouc@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/14 10:07:40 by oakhmouc          #+#    #+#             */
+/*   Updated: 2025/10/14 11:23:08 by oakhmouc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-int	da_init(t_dynamic_array *da, size_t count)
+#include "include.h"
+
+int	da_init(t_dynamic_array da, size_t count)
 {
-	da->size = sizeof(char *);
-	da->count = count;
-	da->array = malloc(da->size * da->count);
-	if (!da->array)
+	da.count = count;
+	da.size = sizeof(char *);
+	da.array = malloc(da.size * da.count);
+	if (!da.array)
 		return (1);
-	ft_memset(da->array, 0, da->count * da->size);
+	ft_memset(da.array, 0, da.count * da.size);
 	return (0);
 }
 
-int	da_append(t_dynamic_array *da, char *s)
+int	da_append(t_dynamic_array da, char *s)
 {
 	t_dynamic_array	tmp;
 	int				index;
 
 	index = 0;
-	if (da && da->count == 2 && !da->array[0])
-		da->array[0] = ft_strdup(s);
+	tmp.array = NULL;
+	if (da.count == 2 && !da.array[0])
+		da.array[0] = ft_strdup(s);
 	else
 	{
-		da_init(&tmp, da->count);
-		while (da->array[index++])
-			tmp.array[index] = ft_strdup(da->array[index]);
+		da_init(tmp, da.count);
+		while (da.array[index++])
+			tmp.array[index] = ft_strdup(da.array[index]);
 		da_free(da);
 		da_init(da, tmp.count + 1);
 		index = 0;
 		while (tmp.array[index++])
-			da->array[index] = ft_strdup(tmp.array[index]);
-		da->array[index] = ft_strdup(s);
-		da_free(&tmp);
+			da.array[index] = ft_strdup(tmp.array[index]);
+		da.array[index] = ft_strdup(s);
+		da_free(tmp);
 	}
 	return (0);
 }
 
-void	da_free(t_dynamic_array *da)
+void	da_free(t_dynamic_array da)
 {
 	int	index;
 
 	index = 0;
-	if (da->array)
+	if (da.array)
 	{
-		while (da->array[index++])
-			free(da->array[index]);
-		free(da->array[index]);
-		free(da->array);
+		while (da.array[index++])
+			free(da.array[index]);
+		free(da.array[index]);
+		free(da.array);
 	}
 }
