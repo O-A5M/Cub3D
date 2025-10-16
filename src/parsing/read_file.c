fc5591da5a13d0af7e7060137806eb26c689ae66
O-A5M <akhmouchothman@gmail.com>
2025-10-16 18:32:54 +0100

    the first of parsing is done

--- src/parsing/read_file.c
+++ src/parsing/read_file.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   read_file.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oakhmouc <oakhmouc@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/16 16:09:15 by oakhmouc          #+#    #+#             */
+/*   Updated: 2025/10/16 16:10:25 by oakhmouc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "include.h"
 #include <stddef.h>
 
@@ -8,7 +20,7 @@ size_t	file_len(int fd)
 
 	ret = 0;
 	tmp = get_next_line(fd);
-	while(tmp)
+	while (tmp)
 	{
 		ret++;
 		free(tmp);
@@ -27,7 +39,7 @@ char	**read_file(int fd, size_t len)
 	tmp = NULL;
 	if (len == 0)
 		return (NULL);
-	ret = malloc(sizeof(char*) * (len + 1));
+	ret = malloc(sizeof(char *) * (len + 1));
 	if (!ret)
 		return (NULL);
 	while (len - index > 0)
