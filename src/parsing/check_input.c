acc389a08329c88ae8d757d3857304518fc37548
O-A5M <akhmouchothman@gmail.com>
2025-11-12 15:16:56 +0100

    checkpoint

--- src/parsing/check_input.c
+++ src/parsing/check_input.c
@@ -6,11 +6,12 @@
 /*   By: oakhmouc <oakhmouc@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/27 12:47:48 by oakhmouc          #+#    #+#             */
-/*   Updated: 2025/11/11 07:07:45 by oakhmouc         ###   ########.fr       */
+/*   Updated: 2025/11/12 11:57:10 by oakhmouc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "include.h"
+#include <stddef.h>
 
 int	check_xpm_exist(t_map *map)
 {
@@ -33,6 +34,50 @@ int	check_xpm_exist(t_map *map)
 		return (-1);
 	close(fd);
 	return (0);
+}
+
+void	add_spaces(char **str, int longest)
+{
+	char	tmp[longest + 1];
+	int		index;
+
+	index = 0;
+	while ((*str)[index])
+	{
+		tmp[index] = (*str)[index];
+		index++;
+	}
+	while (index < longest)
+	{
+		tmp[index] = '7';
+		index++;
+	}
+	tmp[index] = '\0';
+	free((*str));
+	(*str) = ft_strdup(tmp);
+}
+
+int	complete_the_map(char **map, int count)
+{
+	int		len;
+	size_t	longest;
+
+	len = 0;
+	longest = 0;
+	while (len < count - 3)
+	{
+		if (longest < ft_strlen(map[len]))
+			longest = ft_strlen(map[len]);
+		len++;
+	}
+	len = 0;
+	while (len < count - 3)
+	{
+		if (ft_strlen(map[len]) < longest)
+			add_spaces(&map[len], longest);
+		len++;
+	}
+	return (longest);
 }
 
 int	is_player_valid(t_map **map)
@@ -71,13 +116,21 @@ int	is_player_valid(t_map **map)
 	return (0);
 }
 
-int	is_map_closed(char **map, int row, int col);
+int	is_map_closed(char **map, int row, int col, int	count)
+{
+	// int	longest;
+
+	(void)row;
+	(void)col;
+	complete_the_map(map, count);
+	return (0);
+}
 
 int	is_map_valide(t_map *map)
 {
 	if (is_player_valid(&map) == -1)
 		return (-1);
-	if (is_map_closed(map->map->array, map->player_pos[0], map->player_pos[1]) == -1)
+	if (is_map_closed(map->map->array, map->player_pos[0], map->player_pos[1], map->map->count) == -1)
 		return (-1);
 	return (0);
 }
